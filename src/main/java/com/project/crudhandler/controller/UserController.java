266a1c407d3ee87faf0b9486bd6a9bcb9fd771a4
Nitik <NitikRanjan@gmail.com>
2024-06-17 14:37:56 +0530

    Method  Add createUser method in UserController

--- src/main/java/com/project/crudhandler/controller/UserController.java
+++ src/main/java/com/project/crudhandler/controller/UserController.java
@@ -2,8 +2,10 @@ package com.project.crudhandler.controller;
 
 import com.project.crudhandler.model.User;
 import com.project.crudhandler.service.UserServiceI;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,8 @@ public class UserController {
     private UserServiceI userServiceI;
 
     @PostMapping("/")
-    public ResponseEntity<User> createUser(){
-        return null;
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userServiceI.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.OK);
     }
 }
