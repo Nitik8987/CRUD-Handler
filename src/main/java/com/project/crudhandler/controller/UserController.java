803a0b69fec4690f8ad1beae8a2294194e3b9aa8
Nitik <NitikRanjan@gmail.com>
2024-06-17 13:42:27 +0530

    commit for create user api

--- src/main/java/com/project/crudhandler/controller/UserController.java
+++ src/main/java/com/project/crudhandler/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.project.crudhandler.controller;
 
+import com.project.crudhandler.model.User;
+import com.project.crudhandler.service.UserServiceI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+    private UserServiceI userServiceI;
+
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(){
+        return null;
+    }
 }
--- src/main/java/com/project/crudhandler/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/crudhandler/service/impl/UserServiceImpl.java
@@ -4,29 +4,37 @@ import com.project.crudhandler.model.User;
 import com.project.crudhandler.repository.UserRepository;
 import com.project.crudhandler.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserServiceI {
 
     @Autowired
     private UserRepository userRepository;
 
-
-
     @Override
     public User createUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(User user, Long userId) {
-        return null;
+        User existingUser = userRepository.findById(userId).orElseThrow(() ->
+//                new RuntimeException("Resource not found on server with ID: " + userId)
+//        );
+//
+//        existingUser.setUserName(user.getUserName());
+//        existingUser.setUserAge(user.getUserAge());
+//        existingUser.setAbout(user.getAbout());
+//        return userRepository.save(existingUser);
     }
 
     @Override
     public User getSingleUser(Long userId) {
-        return null;
+//        return userRepository.findById(userId)
+//                .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
     }
 
     @Override
@@ -36,11 +44,13 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public void deleteUser(Long userId) {
-
+//        User user = userRepository.findById(userId)
+//                .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
+//        userRepository.delete(user);
     }
 }
