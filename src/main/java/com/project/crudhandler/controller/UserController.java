7714ab776799bddbc8766de146669fe42dfe89c9
Nitik <NitikRanjan@gmail.com>
2024-06-19 18:57:19 +0530

    Added Logger in UserController and UserServiceImpl

--- src/main/java/com/project/crudhandler/controller/UserController.java
+++ src/main/java/com/project/crudhandler/controller/UserController.java
@@ -2,35 +2,42 @@ package com.project.crudhandler.controller;
 
 import com.project.crudhandler.model.User;
 import com.project.crudhandler.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
     private UserServiceI userServiceI;
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        log.info("Creating user: {}", user);
         User savedUser = userServiceI.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.OK);
     }
 
     @GetMapping("/users/{userId}")
     public ResponseEntity<User> getSingleUser(@PathVariable Long userId) {
+        log.info("Fetching user with ID: {}", userId);
         User user = userServiceI.getSingleUser(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/users/{userId}")
     public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long userId) {
+        log.info("Updating user with ID: {}", userId);
         User updatedUser = userServiceI.updateUser(user, userId);
         return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
     }
+
     @DeleteMapping("/users/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        log.info("Deleting user with ID: {}", userId);
         userServiceI.deleteUser(userId);
-        return new ResponseEntity<>("Resource Deleted Successfully !!",HttpStatus.OK);
+        return new ResponseEntity<>("Resource Deleted Successfully !!", HttpStatus.OK);
     }
 }
--- src/main/java/com/project/crudhandler/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/crudhandler/service/impl/UserServiceImpl.java
@@ -3,12 +3,14 @@ package com.project.crudhandler.service.impl;
 import com.project.crudhandler.model.User;
 import com.project.crudhandler.repository.UserRepository;
 import com.project.crudhandler.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserServiceI {
 
     @Autowired
@@ -16,11 +18,13 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public User createUser(User user) {
+        log.info("Saving new user: {}", user);
         return userRepository.save(user);
     }
 
     @Override
     public User updateUser(User user, Long userId) {
+        log.info("Updating user with ID: {}", userId);
         User existingUser = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
 
@@ -32,24 +36,28 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public User getSingleUser(Long userId) {
+        log.info("Fetching user with ID: {}", userId);
         return userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
     }
 
     @Override
     public List<User> getAllUser() {
+        log.info("Fetching all users");
         return userRepository.findAll();
     }
 
     @Override
     public List<User> getAllUsers() {
+        log.info("Fetching all users");
         return userRepository.findAll();
     }
 
     @Override
     public void deleteUser(Long userId) {
-         User user = userRepository.findById(userId)
-                 .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
-         userRepository.delete(user);
+        log.info("Deleting user with ID: {}", userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Resource not found on server with ID: " + userId));
+        userRepository.delete(user);
     }
 }
